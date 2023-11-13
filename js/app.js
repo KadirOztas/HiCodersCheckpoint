6e3913e70d3a2e2d9b2e2cdf930c0405abff7cde
KadirOztas <m.abdulkadiroztas@gmail.com>
2023-11-13 18:13:29 +0100

    I added branch value in students objects. I created a new div and made some arrangements in "style.css" file.

--- js/app.js
+++ js/app.js
@@ -1,23 +1,26 @@
 const school = [
   {
+    id: 1,
     title: "teachers",
     details: [
       { name: "Hannes Bühler", expertise: "Javascript" },
       { name: "Ali Sayar", expertise: "AWS" },
     ],
-    },
-    {
-        title: "students",
-        details: [
-            { name: "Max Hermann", branhc: "Fullstack", average_grade: 5.4 },
-            { name: "Anthony Egbe", branhc: "Cloud", average_grade: 4.9 }
-        ]
-    },
-    {
-        title: "classes",
-        details: [
-            { class: "Fullstack" },
-            {class: "Cloud"}
-        ]
-    }
+  },
+  {
+    id: 2,
+    title: "students",
+    details: [
+      { name: "Max Hermann", branch: "Fullstack", average_grade: 5.4 },
+      { name: "Anthony Egbe", branch: "Cloud", average_grade: 4.9 },
+    ],
+  },
+  {
+    id: 3,
+    title: "classes",
+    details: [{ class: "Fullstack" }, { class: "Cloud" }],
+  },
 ];
+// I need to make a commit about id situation too.
+
+import { renderNav } from "./nav.js";
---
+++ js/nav.js
@@ -0,0 +1,12 @@
+
+
+const renderNav = () => {
+    const app = document.getElementById("app");
+    app.innerHTML += `
+    <div class="nav">
+    </div>
+    `
+}
+
+renderNav()
+export{renderNav}
